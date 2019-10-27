bcc5ba4835c7d4af13dfc1375a512cbd5f4f8ccb
David <davebrown211@gmail.com>
2019-10-27 12:56:47 -0500

    initial commit

---
+++ web/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './assets/trollFace.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Dre Flores
+        </p>
+        <p>
+          Get on my level
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
