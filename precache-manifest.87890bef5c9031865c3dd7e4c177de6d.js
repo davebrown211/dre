1d9265162eb9d224a07f095c2e373a7109e1029a
David <davebrown211@gmail.com>
2019-10-27 12:53:18 -0500

    Updates

---
+++ precache-manifest.87890bef5c9031865c3dd7e4c177de6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d0faa309108aee764961acc1ce2a27",
+    "url": "/dre/index.html"
+  },
+  {
+    "revision": "2a16c1863cb643f93830",
+    "url": "/dre/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61b38d9f4ebd173971e6",
+    "url": "/dre/static/js/2.7424dee9.chunk.js"
+  },
+  {
+    "revision": "2a16c1863cb643f93830",
+    "url": "/dre/static/js/main.19fd8038.chunk.js"
+  },
+  {
+    "revision": "ceeb49b4ee7eb146ba72",
+    "url": "/dre/static/js/runtime-main.e161f6c6.js"
+  },
+  {
+    "revision": "c5093d2343f3e2a82f76ad0666f1ac02",
+    "url": "/dre/static/media/trollFace.c5093d23.jpg"
+  }
+]);
